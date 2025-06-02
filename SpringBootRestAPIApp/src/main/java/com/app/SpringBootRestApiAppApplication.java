b462dd21abd1db39631391b24888d3bc9c71846c
bmayuri20 <bmayuri100@gmail.com>
2025-06-02 15:42:04 +0530

    Initial commit

---
+++ SpringBootRestAPIApp/src/main/java/com/app/SpringBootRestApiAppApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiAppApplication.class, args);
+	}
+
+}
