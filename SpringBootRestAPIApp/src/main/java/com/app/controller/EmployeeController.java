8b8fd0dd7510bcec1d0addfba26690383a6200a7
bmayuri20 <bmayuri100@gmail.com>
2025-06-03 11:19:25 +0530

    adding functionalities

--- SpringBootRestAPIApp/src/main/java/com/app/controller/EmployeeController.java
+++ SpringBootRestAPIApp/src/main/java/com/app/controller/EmployeeController.java
@@ -1,5 +1,36 @@
 package com.app.controller;
 
-public class EmployeeController {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.model.Employee;
+
+@RestController
+public class EmployeeController {
+	List<Employee> list= new ArrayList<>();
+	
+	@PostMapping("/employee")
+	//@RequestMapping(value= "/employee", method = RequestMethod.POST)
+	public Employee saveEmp(@RequestBody Employee e) {
+		list.add(e);
+		return e;
+	}
+	
+	@GetMapping("/employee")
+	public List<Employee> getEmployees(){
+		return list;
+	}
+	@PutMapping("/employee/{empId}")
+	public Employee updateEmp(@PathVariable("empId") int empId, @RequestBody Employee e) {
+		e.setEmpId(empId);
+		System.out.println("call"+ e.getEmpName());
+		return e;
+}
 }
